00b256401fcf30a453e8f8567198e5f220596326
Chris Stead <cmstead@Chriss-MacBook-Pro.local>
2021-12-28 14:39:26 -0800

    added initial behavior

---
+++ bot.js
@@ -0,0 +1,41 @@
+require('dotenv').config();
+
+const Discord = require('discord.js');
+const client = new Discord.Client({ intents: ["GUILDS", "GUILD_MESSAGES"] });
+
+const emotes = require('./emotes');
+
+client.on('ready', () => {
+    console.log(`Logged in as ${client.user.tag}!`);
+});
+
+
+function sendReponse(message, tokens) {
+    const phrase = tokens.join(' ');
+    const emote = emotes.find(emote => emote.name.includes(phrase));
+
+    if(tokens.length === 0) {
+        message.reply('you rang?');
+    } else if(phrase === 'list') {
+        const availableEmotes = emotes.map(emote => emote.name);
+        message.reply(`
+Here are the emotes I know:
+
+${availableEmotes.join('\n')}
+        `)
+    } else if(typeof emote === 'object'){
+        message.reply(emote.url);
+    } else {
+        message.reply('I don\'t know that one!')
+    }
+}
+
+client.on('message', msg => {
+    const tokens = msg.content.toLowerCase().split(' ');
+
+    if(tokens[0] === 'scotl') {
+        sendReponse(msg, tokens.slice(1));
+    }
+});
+
+client.login(process.env.DISCORD_BOT_TOKEN);
---
+++ emotes.js
@@ -0,0 +1,106 @@
+module.exports = [
+    {
+        name: 'wipe brow exhausted doc worker',
+        url: 'https://static.wikia.nocookie.net/sky-children-of-the-light/images/6/6f/ExhaustedDockWorker-1.png/revision/latest/scale-to-width-down/300?cb=20210702182156'
+    },
+    {
+        name: 'tired slumbering shipwright',
+        url: 'https://static.wikia.nocookie.net/sky-children-of-the-light/images/b/bb/SlumberingShipwright-1.png/revision/latest/scale-to-width-down/300?cb=20210702182129'
+    },
+    {
+        name: 'wave waving bellmaker',
+        url: 'https://static.wikia.nocookie.net/sky-children-of-the-light/images/0/0d/WavingBellmaker-1.png/revision/latest/scale-to-width-down/300?cb=20210702182118'
+    },
+    {
+        name: 'laughing light catcher',
+        url: 'https://static.wikia.nocookie.net/sky-children-of-the-light/images/4/4e/LaughingLightCatcher-1.png/revision/latest/scale-to-width-down/300?cb=20210702182144'
+    },
+    {
+        name: 'clapping applauding bellmaker',
+        url: 'https://static.wikia.nocookie.net/sky-children-of-the-light/images/2/25/ApplaudingBellmaker-1.png/revision/latest/scale-to-width-down/300?cb=20210702182107'
+    },
+    {
+        name: 'shivering trailblazer',
+        url: 'https://static.wikia.nocookie.net/sky-children-of-the-light/images/3/35/ShiveringTrailblazer-1.png/revision/latest/scale-to-width-down/300?cb=20210718190625'
+    },
+    {
+        name: 'blushing prospector',
+        url: 'https://static.wikia.nocookie.net/sky-children-of-the-light/images/6/6f/BlushingProspector-1.png/revision/latest/scale-to-width-down/300?cb=20210718190805'
+    },
+    {
+        name: 'pouting pouty porter',
+        url: 'https://static.wikia.nocookie.net/sky-children-of-the-light/images/f/f7/PoutyPorter-1.png/revision/latest/scale-to-width-down/300?cb=20210718190859'
+    },
+    {
+        name: 'dismayed hunter',
+        url: 'https://static.wikia.nocookie.net/sky-children-of-the-light/images/7/7e/DismayedHunter-1.png/revision/latest/scale-to-width-down/300?cb=20210718190947'
+    },
+    {
+        name: 'apologetic lumberjack',
+        url: 'https://static.wikia.nocookie.net/sky-children-of-the-light/images/2/23/ApologeticLumberjack-1.png/revision/latest/scale-to-width-down/300?cb=20210718191028'
+    },
+    {
+        name: 'crying tearful light miner',
+        url: 'https://static.wikia.nocookie.net/sky-children-of-the-light/images/9/9c/TearfulLightMiner-1.png/revision/latest/scale-to-width-down/300?cb=20210718191148'
+    },
+    {
+        name: 'handstanding thrillseeker',
+        url: 'https://static.wikia.nocookie.net/sky-children-of-the-light/images/0/09/HandstandingThrillseeker-1.png/revision/latest/scale-to-width-down/300?cb=20210718192124'
+    },
+    {
+        name: 'bowing medalist',
+        url: 'https://static.wikia.nocookie.net/sky-children-of-the-light/images/b/b2/BowingMedalist-1.png/revision/latest/scale-to-width-down/300?cb=20210718192236'
+    },
+    {
+        name: 'backflipping champion',
+        url: 'https://static.wikia.nocookie.net/sky-children-of-the-light/images/7/75/BackflippingChampion-1.png/revision/latest/scale-to-width-down/300?cb=20210718192148'
+    },
+    {
+        name: 'cheering cheerful spectator',
+        url: 'https://static.wikia.nocookie.net/sky-children-of-the-light/images/d/df/CheerfulSpectator-1.png/revision/latest/scale-to-width-down/300?cb=20210718192214'
+    },
+    {
+        name: 'fainting warrior',
+        url: 'https://static.wikia.nocookie.net/sky-children-of-the-light/images/8/87/FaintingWarrior-1.png/revision/latest/scale-to-width-down/300?cb=20210718193338'
+    },
+    {
+        name: 'frightened refugee',
+        url: 'https://static.wikia.nocookie.net/sky-children-of-the-light/images/f/f9/FrightenedRefugee-1.png/revision/latest/scale-to-width-down/300?cb=20210718193429'
+    },
+    {
+        name: 'saluting captain',
+        url: 'https://static.wikia.nocookie.net/sky-children-of-the-light/images/2/2b/SalutingCaptain-1.png/revision/latest/scale-to-width-down/300?cb=20210718193759'
+    },
+    {
+        name: 'lookout scout',
+        url: 'https://static.wikia.nocookie.net/sky-children-of-the-light/images/f/fa/LookoutScout-1.png/revision/latest/scale-to-width-down/300?cb=20210718193823'
+    },
+    {
+        name: 'praying acolyte',
+        url: 'https://static.wikia.nocookie.net/sky-children-of-the-light/images/7/75/PrayingAcolyte-1.png/revision/latest/scale-to-width-down/300?cb=20210718200213'
+    },
+    {
+        name: 'meditating monastic',
+        url: 'https://static.wikia.nocookie.net/sky-children-of-the-light/images/a/a5/Meditating-Monastic-1.png/revision/latest/scale-to-width-down/300?cb=20210718200002'
+    },
+    {
+        name: 'levitating adept',
+        url: 'https://static.wikia.nocookie.net/sky-children-of-the-light/images/f/f2/LevitatingAdept-1.png/revision/latest/scale-to-width-down/300?cb=20210718200036'
+    },
+    {
+        name: 'confident sightseer',
+        url: 'https://static.wikia.nocookie.net/sky-children-of-the-light/images/c/c8/ConfidentSightseer-1.png/revision/latest/scale-to-width-down/300?cb=20210718192058'
+    },
+    {
+        name: 'proud victor',
+        url: 'https://static.wikia.nocookie.net/sky-children-of-the-light/images/d/dc/ProudVictor-1.png/revision/latest/scale-to-width-down/300?cb=20210718192254'
+    },
+    {
+        name: 'stealthy survivor',
+        url: 'https://static.wikia.nocookie.net/sky-children-of-the-light/images/f/f1/StealthySurvivor-1.png/revision/latest/scale-to-width-down/300?cb=20210718193735'
+    },
+    {
+        name: 'polite scholar',
+        url: 'https://static.wikia.nocookie.net/sky-children-of-the-light/images/3/3e/PoliteScholar-1.png/revision/latest/scale-to-width-down/300?cb=20210718200359'
+    },
+];
