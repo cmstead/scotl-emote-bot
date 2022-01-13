72f07b992def3e434b02b3677fdd7b961a4ac1a3
Chris Stead <cmstead@Chriss-MacBook-Pro.local>
2022-01-12 20:38:17 -0800

    F Added help info about next reset command

--- help.js
+++ help.js
@@ -8,11 +8,12 @@ next
 
 Next Event (next):
 
-You can ask about the next Grandma or Geyser event in the following ways.
+You can ask about the next Grandma, Geyser, or Daily Reset event in the following ways.
 
 scotl next grandma
 scotl next gma
 scotl next geyser
+scotl next reset
 
 Show Emote:
 
