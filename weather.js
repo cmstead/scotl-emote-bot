ec032dd1852ed12d2fe323c21253017fb7c1a2bd
Chris Stead <cmstead@Chriss-MacBook-Pro.local>
2022-01-26 21:16:35 -0800

    F Weather reports midday change on Wednesdays

--- weather.js
+++ weather.js
@@ -3,9 +3,13 @@ const { getCurrentPacificDay } = require('./datetime');
 function getWeather() {
     const currentDay = getCurrentPacificDay();
 
-    return currentDay < 4
-        ? 'Today at the Treehouse we can expect to see some rain'
-        : 'It\'ll be cloudy, but dry today at the Treehouse';
+    if(currentDay < 3) {
+        return 'Today at the Treehouse we can expect to see some rain'
+    } else if (currentDay > 3) {
+        return 'It\'ll be cloudy, but dry today at the Treehouse';
+    } else {
+        return 'Expect rain in the morning, which should clear by early afternoon'
+    }
 }
 
 module.exports = {
