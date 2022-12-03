1785a65532ca02b38b497a94c38032fe07b0f5e0
Chris Stead <cmstead@Chriss-MBP.attlocal.net>
2022-12-02 21:38:08 -0800

    F!! Added simple server to check status

--- bot.js
+++ bot.js
@@ -1,4 +1,5 @@
 require('dotenv').config();
+const http = require('http');
 
 const { Client } = require('discord.js');
 const client = new Client({ intents: ["GUILDS", "GUILD_MESSAGES", "GUILD_MEMBERS"] });
@@ -6,9 +7,30 @@ const client = new Client({ intents: ["GUILDS", "GUILD_MESSAGES", "GUILD_MEMBERS
 const { sendReponse } = require('./responder');
 const startAlertTimer = require('./alerter')(client);
 
+const port = process.env.PORT ?? 8080;
+
+let clientName = null;
+
+function requestListener(_, response) {
+    const message = clientName === null
+        ? 'Unable to connect'
+        : `Worker Started: ${clientName}`;
+
+    response.writeHead(200);
+    response.end(message);
+}
+
+const server = http.createServer(requestListener);
+
+server.listen(port, function () {
+    console.log('Server started');
+})
+
 client.login(process.env.DISCORD_BOT_TOKEN);
 
 client.on('ready', () => {
+    clientName = client.user.tag;
+    
     console.log(`Logged in as ${client.user.tag}!`);
 
     startAlertTimer();
