1f2daa654627d887921ac8fa51018f3cb2fdc775
Chris Stead <cmstead@gmail.com>
2025-01-26 01:15:30 -0800

    F Added messaging for emotes!

--- responder.js
+++ responder.js
@@ -25,7 +25,7 @@ ${waveUrl}`);
         const channel = guild.channels.cache.get(message.channelId);
         
         channel.send({
-            content: `<@${message.author.id}> says:\n${emote.url}`
+            content: `<@${message.author.id}> says: ${tokens.slice(2).join(' ')}\n${emote.url}`
         });
         message.delete();
     } else if (firstToken === 'next') {
