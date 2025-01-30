9174ad61029757d77e92e2d4a4faf57f01b1289a
Chris Stead <cmstead@gmail.com>
2025-01-30 09:48:43 -0800

    F Excluding scotl bot from reactions

--- react-and-reply.js
+++ react-and-reply.js
@@ -1,32 +1,34 @@
 function reactAndReply(msg, tokens) {
-    if ((/[^a-z]*h+(j|y)*(o+|ö+|a(w|h)+)n+k+/ig).test(msg.content)) {
-        msg.react('<:honk:1178124672861225000>');
-    }
+    if (msg.author.id !== '925464580644294707') {
+        if ((/[^a-z]*h+(j|y)*(o+|ö+|a(w|h)+)n+k+/ig).test(msg.content)) {
+            msg.react('<:honk:1178124672861225000>');
+        }
 
-    if ((/[ck]rab/ig).test(msg.content)) {
-        msg.react('<:dedkrab:1193212738558763029>');
-    }
+        if ((/[ck]rab/ig).test(msg.content)) {
+            msg.react('<:dedkrab:1193212738558763029>');
+        }
 
-    if ((/krill/ig).test(msg.content)) {
-        msg.react('<:pkrilleye:1062746661438369823>');
-    }
+        if ((/krill/ig).test(msg.content)) {
+            msg.react('<:pkrilleye:1062746661438369823>');
+        }
 
-    if ((/mo(th|ff)/ig).test(msg.content)) {
-        msg.react('<:mothhonk:1191906094256238703>');
-    }
+        if ((/mo(th|ff)/ig).test(msg.content)) {
+            msg.react('<:mothhonk:1191906094256238703>');
+        }
 
-    const phrases = [
-        'Honk?',
-        'Honk!',
-        'Sorry, I was busy reading your messages.',
-        'https://media1.tenor.com/m/qSFhB10WHrYAAAAd/sky-cotl-heart-hands.gif',
-        'https://media1.tenor.com/m/MUtpsIlO17IAAAAd/sky-cotl-faint.gif',
-        'https://media1.tenor.com/m/rlDth0qT_wMAAAAd/sky-cotl-float.gif',
-    ];
+        const phrases = [
+            'Honk?',
+            'Honk!',
+            'Sorry, I was busy reading your messages.',
+            'https://media1.tenor.com/m/qSFhB10WHrYAAAAd/sky-cotl-heart-hands.gif',
+            'https://media1.tenor.com/m/MUtpsIlO17IAAAAd/sky-cotl-faint.gif',
+            'https://media1.tenor.com/m/rlDth0qT_wMAAAAd/sky-cotl-float.gif',
+        ];
 
-    if(tokens.includes('<@925464580644294707>')){
-        const messageIndex = Math.floor(Math.random() * phrases.length);
-        msg.reply(phrases[messageIndex]);
+        if (tokens.includes('<@925464580644294707>')) {
+            const messageIndex = Math.floor(Math.random() * phrases.length);
+            msg.reply(phrases[messageIndex]);
+        }
     }
 }
 
