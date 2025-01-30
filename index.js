441a72d3d38f309161d5c246c2c5d235199e0da6
Chris Stead <cmstead@gmail.com>
2025-01-29 23:47:05 -0800

    B Still trying to get that honk

--- index.js
+++ index.js
@@ -52,7 +52,7 @@ client.on('messageCreate', msg => {
 
     if (['scotl', 'scott'].includes(firstTokenLc)) {
         sendReponse(msg, tokens.slice(1), client);
-    } else if((/[^a-z]hj?onk/ig).test(msg.content)) {
+    } else if((/[^a-z]*hj?onk/ig).test(msg.content)) {
         msg.reply('Honk!');
     } else if (msg.channelId === submitATipChannelId) {
         moveTip.move(msg);
