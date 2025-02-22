12a15336f56255025658f88925435bd5800a61b0
Chris Stead <cmstead@gmail.com>
2025-02-22 00:56:02 -0800

    Revert "F Switched output channel to sky-general"
    This reverts commit bebfc161a50e7d96ebba56a2450c252aa76725f0.

--- alerter.js
+++ alerter.js
@@ -8,6 +8,25 @@ const oneMinuteInMs = 60 * 1000;
 
 module.exports = function (client) {
 
+    function sendDirectMessageAlert(alertMessage) {
+        client.guilds.cache.forEach((guild) => {
+            guild.fetch()
+                .then((fetchedGuild) => {
+                    return fetchedGuild.members.fetch();
+                })
+                .then(members =>
+                    members.filter((member) => {
+                        return member.roles.cache.find(role => role.name === SCOTL_ALERT_ROLE)
+                    }))
+                .then((filteredMembers) => {
+                    filteredMembers.forEach(member => member.send(alertMessage));
+                })
+                .catch(function (error) {
+                    console.log(`was there an error: ${error.message}`);
+                });
+        });
+    }
+
     function sendChannelMessageAlert(alertMessage, options = {}) {
         console.log(`send message request received: ${alertMessage}`);
         client.guilds.cache.forEach((guild) => {
@@ -18,7 +37,7 @@ module.exports = function (client) {
                 .then((channels) => {
                     console.log('attempting to send to channel');
 
-                    const generalChannel = channels.find((channel) => channel.name.endsWith('sky-general'))
+                    const generalChannel = channels.find((channel) => channel.name.endsWith('general'))
 
                     if (generalChannel) {
                         console.log(`sending daily reset message to ${generalChannel.name}`);
@@ -37,8 +56,41 @@ module.exports = function (client) {
         });
     }
 
+
+    function isEvenHour(hour) {
+        return hour % 2 === 0;
+    }
+
+    function isOddHour(hour) {
+        return !isEvenHour(hour);
+    }
+
+    function isNearGeyserTime(hour, minutes) {
+        return isOddHour(hour) && minutes >= 45;
+    }
+
     function isAfterResetTime(hour, minutes) {
         return hour % 24 === 0 && minutes > 5;
+    }
+
+    function isNearForestRainbowTime(hour, minutes) {
+        return [4, 16].includes(hour) && minutes > 54;
+    }
+
+    function isNearFairyRingTime(hour, minutes) {
+        return minutes > 40;
+    }
+
+    function isNearSunsetTime(hour, minutes) {
+        return isEvenHour(hour) && minutes > 40 && minutes < 55;
+    }
+
+    function isOkayToAlert(lastAlert, offset = 20) {
+        return differenceInMilliseconds(new Date(), lastAlert) > offset * oneMinuteInMs;
+    }
+
+    function getTimeToNextHour(minutes) {
+        return 60 - minutes;
     }
 
     function isAfterEdenReset(hour, minutes) {
@@ -51,8 +103,22 @@ module.exports = function (client) {
         return getCurrentGMTDay() === 6;
     }
 
+    // let lastGeyserAlert = new Date();
+    // let lastRainbowAlert = new Date();
+    // let lastSunsetAlert = new Date();
+    // let lastFairyRingAlert = new Date();
     let lastFashionDayAlert = new Date();
     let lastGeneralChannelResetAlert = new Date();
+
+    function pluralize(time, word) {
+        return time === 1 ? word : `${word}s`;
+    }
+
+    function isRandomAlertTime(period, frequency) {
+        const selectedValue = Math.floor(Math.random() * period);
+
+        return selectedValue < frequency;
+    }
 
     return function startAlertTimer() {
         setInterval(() => {
@@ -60,6 +126,30 @@ module.exports = function (client) {
             const timeTokens = currentTime.split(':');
             const hour = parseInt(timeTokens[0]);
             const minutes = parseInt(timeTokens[1]);
+
+            // let messagesToSend = [];
+
+            // if (isNearGeyserTime(hour, minutes) && isOkayToAlert(lastGeyserAlert)) {
+            //     const geyserMinutes = getTimeToNextHour(minutes) + 5;
+            //     const grandmaMinutes = geyserMinutes + 30;
+
+            //     messagesToSend.push(`The polluted geyser is erupting in ${geyserMinutes} ${pluralize(geyserMinutes, 'minute')}`)
+            //     messagesToSend.push(`Grandma is serving a meal in ${grandmaMinutes} ${pluralize(grandmaMinutes, 'minute')}`)
+
+            //     lastGeyserAlert = new Date();
+            // }
+
+            // if (isRandomAlertTime(3, 1) && isOkayToAlert(lastRainbowAlert) && isNearForestRainbowTime(hour, minutes)) {
+            //     sendChannelMessageAlert('Visit the forest brook _soon_ for something beautiful!');
+            //     lastRainbowAlert = new Date();
+            // }
+
+            // const twelveHours = 12 * 60;
+
+            // if (isRandomAlertTime(25, 1) && isOkayToAlert(lastFairyRingAlert, twelveHours) && isNearFairyRingTime(hour, minutes)) {
+            //     sendChannelMessageAlert('Visit the hill above the 8-player door _soon_ for a little magic!');
+            //     lastFairyRingAlert = new Date();
+            // }
 
             const twentyFourHours = 24 * 60
 
@@ -71,6 +161,12 @@ module.exports = function (client) {
                     },
                 });
             }
+
+            // if (isRandomAlertTime(25, 1) && isOkayToAlert(lastSunsetAlert, twelveHours) && isNearSunsetTime(hour, minutes)) {
+            //     sendChannelMessageAlert('Visit Sanctuary Islands _soon_ for something wonderful!');
+
+            //     lastSunsetAlert = new Date();
+            // }
 
             if (isAfterResetTime(hour, minutes) && isOkayToAlert(lastGeneralChannelResetAlert, 60)) {
                 let resetAlertMessage = [
